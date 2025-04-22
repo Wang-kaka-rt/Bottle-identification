ed169ac637c7cce3cf80745830a7adca53610260
Wang-kaka-rt <18661987297@163.com>
2025-04-22 17:44:59 +0200

    Add files via upload

--- yolo/train.py
+++ yolo/train.py
@@ -1,21 +1,22 @@
+import torch
 from ultralytics import YOLO
-
+if __name__ == "__main__":
 # 初始化YOLOv8模型
-model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # 从官方配置构建并加载预训练权重
+    model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # 从官方配置构建并加载预训练权重
 
 # 开始训练
-results = model.train(
-    data='/Users/wangzhenyu/Desktop/Project-AI/Opencv/yolo/data.yaml',
-    epochs=20,
-    batch=8,
+    results = model.train(
+    data='D:/resource-ai/Bottle-identification/yolo/data.yaml',
+    epochs=200,
+    batch=256,
     imgsz=320,
-    device='mps',
+    device='0',
     workers=2,
     optimizer='Adam',
     lr0=0.001,
     name='model',
     project='models',
-    fraction=0.074  # 4000 / 13689 (精确计算后的样本比例)
-)
+    fraction=1  # 4000 / 13689 (精确计算后的样本比例)
+    )
 
-print(f'训练完成，最佳模型保存在：{results.save_dir}')
+    print(f'训练完成，最佳模型保存在：{results.save_dir}')
